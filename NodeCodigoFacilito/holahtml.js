d23cd2fe575b08b93f04d784983611a13aa0b514
themostretarded <marcosresidentevil@hotmail.com>
2018-01-30 21:16:38 -0600

    Guardado2

--- NodeCodigoFacilito/holahtml.js
+++ NodeCodigoFacilito/holahtml.js
@@ -1,11 +1,17 @@
 var http = require("http"),
     fs = require("fs");
 
-var html = fs.readFileSync("./index.html",function(err,html){
-    
+var html = fs.readFile("./index.html",function(err,html){
+    res.write(html);
+    res.end("se cerro");
 });
 
 http.createServer(function(req,res){
-    res.write(html);
-    res.end();
+   
 }).listen(8081);
+
+/*shity
+var html = fs.readFileSync("./index.html",function(err,html){
+    
+});
+*/

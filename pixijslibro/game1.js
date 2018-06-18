4ad9d2fda5197178d40f5f31e281181328cfe0d9
themostretarded <marcosresidentevil@hotmail.com>
2018-06-18 14:57:37 -0500

    Limpienado todo

--- pixijslibro/game1.js
+++
@@ -1,8 +0,0 @@
-let renderer = PIXI.autoDetectRenderer(256, 256);
-document
-    .body
-    .appendChild(renderer.view);
-let stage = new PIXI.Container();
-renderer.render(stage);
-let scale = scaleToWindow(renderer.view);
-console.log(scale);
